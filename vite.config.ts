63ad33efe3037df539e95417b43bf45076f97762
turabunase <turabunase@gmail.com>
2025-07-13 15:50:34 +0200

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+ import path from 'path';
+ import { fileURLToPath } from 'url';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/cartolibreria-fantasia/', // Sostituisci con il nome del tuo repository se diverso
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.'),
         }
       }
     };
